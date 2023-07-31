de2b734aa1f4ce0a26ca725506d1217bc041c371
tikikun <daogiatuank54@gmail.com>
2023-07-31 03:00:12 +0000

    sample code... still broken in http

--- src/main.cc
+++ src/main.cc
@@ -4,34 +4,66 @@
 int main()
 {
     // create a bot object with your Discord bot token
-    dpp::cluster my_bot("MTA5MjI0NzExMzg4ODQ0ODUyMg.GyoD2C.nvCoqDhqtjJwmj5_d5-2UC91bwMN4046WBLqmc");
-    dpp::message msg(1093368776910716979, "Are you good?");
-    my_bot.message_create(msg);
+    dpp::cluster dominic("MTA5MjI0NzExMzg4ODQ0ODUyMg.GyoD2C.nvCoqDhqtjJwmj5_d5-2UC91bwMN4046WBLqmc");
+    dominic.on_log(dpp::utility::cout_logger());
+    dpp::message msg(1093368776910716979, "Bot is up!");
+    dominic.message_create(msg);
+    dominic.on_slashcommand([&dominic](const dpp::slashcommand_t &event)
+                            {
+         if (event.command.get_command_name() == "imagine") {
+            std::string prompt = std::get<std::string>(event.get_parameter("prompt"));
+            std::string negative_prompt ;
+            int steps;
+            try {
+            negative_prompt = std::get<std::string>(event.get_parameter("negative_prompt"));
+            steps = std::get<int64_t>(event.get_parameter("steps"));
+            } catch (const std::bad_variant_access&) {
+                negative_prompt = "asdfasdf";
+                steps = 20;
+            }
 
-    my_bot.on_slashcommand([](const dpp::slashcommand_t &event)
-                           {
-         if (event.command.get_command_name() == "ping") {
-            event.reply("Pong!");
-        } });
+            dominic.log(dpp::loglevel::ll_info, "Finished processing an image");
+            dpp::json inference_payload = {
+            {"prompt", prompt},
+            {"neg_prompt", negative_prompt},
+            {"unet_model", "openjourney"},
+            {"seed", 37232013},
+            {"steps", steps}
+        };
 
-    my_bot.on_slashcommand([](const dpp::slashcommand_t &event)
-                           {
-         if (event.command.get_command_name() == "alan_test") {
-            std::string input_testing = std::get<std::string>(event.get_parameter("testing"));
-            event.reply(input_testing);
-        } });
-
-    my_bot.on_ready([&my_bot](const dpp::ready_t &event)
+                std::string payload = R"(
                     {
+                    "prompt": "a",
+                    "neg_prompt": "cat",
+                    "unet_model": "openjourney",
+                    "seed": 12312312,
+                    "steps": 20
+                    }
+                    )";
+            dominic.request(
+            "https://sd-inference.jan.ai/sd_inference", dpp::m_post, [&event](const dpp::http_request_completion_t &cc) {
+                // This callback is called when the HTTP request completes. See documentation of
+                // dpp::http_request_completion_t for information on the fields in the parameter.
+                            std::cout<< cc.status;
+                            event.reply();
+            },
+            payload  // convert the json object to a string
+            ,"text/plain",{{"Content-Type", "application/json"}});
+
+            
+        } });
+
+    dominic.on_ready([&dominic](const dpp::ready_t &event)
+                     {
         if (dpp::run_once<struct register_bot_commands>()) {
-            my_bot.global_command_create(dpp::slashcommand("ping", "Ping pong!", my_bot.me.id));
-            dpp::slashcommand alan_test("alan_test", "Ping pong!", my_bot.me.id);
-            alan_test.add_option(
-                dpp::command_option(dpp::co_string,"testing","The testing to fill here",true)
-            );
-            my_bot.global_command_create(alan_test);
+            dpp::slashcommand imagine("imagine","Type your description of the image you want",dominic.me.id);
+            dpp::command_option prompt_option(dpp::co_string,"prompt","Description of your image",true);
+            dpp::command_option negative_prompt_option(dpp::co_string,"negative_prompt","Things you do not want to see in the image",false);
+            dpp::command_option steps_option(dpp::co_integer,"steps","numbers of diffusion steps",false);
+            imagine.add_option(prompt_option).add_option(negative_prompt_option);
+            dominic.global_command_create(imagine);
         } });
     // start the bot
-    my_bot.start(dpp::st_wait);
+    dominic.start(dpp::st_wait);
     return 0;
 }
