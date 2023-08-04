5df32b2331351dd0a7cd4946090ba121ccc99c51
tikikun <daogiatuank54@gmail.com>
2023-08-04 14:50:11 +0000

    add source and dependency

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,9 @@
+#define CPPHTTPLIB_OPENSSL_SUPPORT
+#include "httplib.h"
 #include <dpp/dpp.h> // include the D++ library
 #include <iostream>
+#include <random>
+#include <chrono>
 
 int main()
 {
@@ -13,44 +17,44 @@ int main()
          if (event.command.get_command_name() == "imagine") {
             std::string prompt = std::get<std::string>(event.get_parameter("prompt"));
             std::string negative_prompt ;
-            int steps;
+            int64_t steps;
             try {
             negative_prompt = std::get<std::string>(event.get_parameter("negative_prompt"));
             steps = std::get<int64_t>(event.get_parameter("steps"));
             } catch (const std::bad_variant_access&) {
-                negative_prompt = "asdfasdf";
+                negative_prompt = "NONE";
                 steps = 20;
             }
+            std::random_device rd;  // Will be used to obtain a seed for the random number engine
+            std::mt19937 gen(rd()); // Standard mersenne_twister_engine seeded with rd()
+            std::uniform_int_distribution<> distrib(0, 10000000);
 
-            dominic.log(dpp::loglevel::ll_info, "Finished processing an image");
+            int random_number = distrib(gen);
             dpp::json inference_payload = {
             {"prompt", prompt},
             {"neg_prompt", negative_prompt},
-            {"unet_model", "openjourney"},
-            {"seed", 37232013},
+            {"unet_model", "meinamix"},
+            {"seed", random_number},
             {"steps", steps}
         };
-
-                std::string payload = R"(
-                    {
-                    "prompt": "a",
-                    "neg_prompt": "cat",
-                    "unet_model": "openjourney",
-                    "seed": 12312312,
-                    "steps": 20
-                    }
-                    )";
-            dominic.request(
-            "https://sd-inference.jan.ai/sd_inference", dpp::m_post, [&event](const dpp::http_request_completion_t &cc) {
-                // This callback is called when the HTTP request completes. See documentation of
-                // dpp::http_request_completion_t for information on the fields in the parameter.
-                            std::cout<< cc.status;
-                            event.reply();
-            },
-            payload  // convert the json object to a string
-            ,"text/plain",{{"Content-Type", "application/json"}});
-
-            
+            std::string result_url;
+            // HTTPS
+            httplib::Client cli("https://sd-inference.jan.ai");
+            httplib::Headers headers = {
+                    {"Content-Type", "application/json"},
+                };
+            auto res = cli.Post("/sd_inference", headers, inference_payload.dump(), "application/json");
+                if (res && res->status == 200) {
+                    result_url = dpp::json::parse(res->body)["url"];
+                } else {
+                    
+                    event.reply("Unkown bug happen, cannot generate image");
+                    return;
+                }
+            std::this_thread::sleep_for(std::chrono::milliseconds(200));
+            dominic.log(dpp::loglevel::ll_info, "Finished processing an image");
+            event.reply(result_url);
+            return;
         } });
 
     dominic.on_ready([&dominic](const dpp::ready_t &event)
@@ -60,7 +64,7 @@ int main()
             dpp::command_option prompt_option(dpp::co_string,"prompt","Description of your image",true);
             dpp::command_option negative_prompt_option(dpp::co_string,"negative_prompt","Things you do not want to see in the image",false);
             dpp::command_option steps_option(dpp::co_integer,"steps","numbers of diffusion steps",false);
-            imagine.add_option(prompt_option).add_option(negative_prompt_option);
+            imagine.add_option(prompt_option).add_option(negative_prompt_option).add_option(steps_option);
             dominic.global_command_create(imagine);
         } });
     // start the bot
