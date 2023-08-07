d4a398e44ef9e6a9224ec8c61b9786ef3e38efb8
tikikun <daogiatuank54@gmail.com>
2023-08-07 05:39:50 +0000

    use config file now

--- src/main.cc
+++ src/main.cc
@@ -4,37 +4,60 @@
 #include <random>
 
 int main() {
+
   unsigned int num_cores(std::thread::hardware_concurrency());
   // create a bot object with your Discord bot token
-  dpp::cluster dominic("MTA5MjI0NzExMzg4ODQ0ODUyMg.GyoD2C.nvCoqDhqtjJwmj5_d5-"
-                       "2UC91bwMN4046WBLqmc",
-                       dpp::i_default_intents, 10);
+
+  std::ifstream file(
+      "/workspace/server_config.json"); // Replace "example.json" with your JSON
+                                        // file's name
+  if (!file.is_open()) {
+    std::cerr << "Error opening the JSON file." << std::endl;
+    return 1;
+  }
+
+  json jsonConfig; // Create a JSON object
+  try {
+    file >> jsonConfig; // Parse the JSON data
+  } catch (json::parse_error &e) {
+    std::cerr << "JSON parse error: " << e.what() << std::endl;
+    return 1;
+  }
+
+  // Server configs go here
+  std::string api_bot_key = jsonConfig["api_bot_key"];
+  std::string negative_prompt_prefix = jsonConfig["negative_prompt_prefix"];
+  std::string configured_unet_model = jsonConfig["unet_model"];
+
+  dpp::cluster dominic(api_bot_key, dpp::i_default_intents, 10);
   dominic.on_log(dpp::utility::cout_logger());
 
-  dominic.on_slashcommand([&dominic](const dpp::slashcommand_t &event) {
+  dominic.on_slashcommand([&dominic, &negative_prompt_prefix,
+                           &configured_unet_model](
+                              const dpp::slashcommand_t &event) {
     if (event.command.get_command_name() == "imagine") {
       std::string prompt = std::get<std::string>(event.get_parameter("prompt"));
       std::string negative_prompt;
       int64_t steps;
       try {
         negative_prompt =
-            "NSFW, sex, porn " +
+            negative_prompt_prefix +
             std::get<std::string>(event.get_parameter("negative_prompt"));
         steps = std::get<int64_t>(event.get_parameter("steps"));
       } catch (const std::bad_variant_access &) {
-        negative_prompt = "NSFW, sex, porn";
+        negative_prompt = negative_prompt_prefix;
         steps = 30;
       }
-      std::random_device
-          rd; // Will be used to obtain a seed for the random number engine
-      std::mt19937 gen(
-          rd()); // Standard mersenne_twister_engine seeded with rd()
+      // Will be used to obtain a seed for the random number engine
+      std::random_device rd;
+      // Standard mersenne_twister_engine seeded with rd()
+      std::mt19937 gen(rd());
       std::uniform_int_distribution<> distrib(0, 10000000);
 
       int random_number = distrib(gen);
       dpp::json inference_payload = {{"prompt", prompt},
                                      {"neg_prompt", negative_prompt},
-                                     {"unet_model", "dreamshaper_7"},
+                                     {"unet_model", configured_unet_model},
                                      {"seed", random_number},
                                      {"steps", steps}};
       std::string inference_payload_string = inference_payload.dump();
